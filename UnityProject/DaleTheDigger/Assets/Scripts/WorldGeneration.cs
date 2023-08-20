5593304a520cb04afb68b0c5cd05afe2155d0a75
lewlol <lewdevbusiness@gmail.com>
2023-08-20 22:09:00 +0100

    WorldGenScript

--- UnityProject/DaleTheDigger/Assets/Scripts/WorldGeneration.cs
+++ UnityProject/DaleTheDigger/Assets/Scripts/WorldGeneration.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class WorldGeneration : MonoBehaviour
 {
+    public Biome underground;
+    public Biome caverns;
+    public Biome theDeep;
+    Biome activeBiome;
+
     public GameObject stone;
     public GameObject[] ores;
     public int worldLength;
     public int worldHeight;
     int currentTile;
+    Vector3 spawnPosition;
     void Start()
     {
         GenerateWorld();
@@ -20,12 +26,19 @@ public class WorldGeneration : MonoBehaviour
         {
             for (int y = 0; y < worldHeight; y++)
             {
-                Vector3 spawnPosition = new Vector3(x, -y, 0);
-                GameObject newTile = Instantiate(stone, spawnPosition, Quaternion.identity);
-                newTile.transform.parent = gameObject.transform;
-                newTile.name = "World Tile " + currentTile;
-                currentTile++;
+                //Check y Level
+                //Compare to Biome Level (if above minimum and below max)
+
+                spawnPosition = new Vector3(x, -y, 0);
             }
         }
+    }
+
+    public void SpawnTile()
+    {
+        GameObject newTile = Instantiate(activeBiome.stone, spawnPosition, Quaternion.identity);
+        newTile.transform.parent = gameObject.transform;
+        newTile.name = "World Tile " + currentTile;
+        currentTile++;
     }
 }
