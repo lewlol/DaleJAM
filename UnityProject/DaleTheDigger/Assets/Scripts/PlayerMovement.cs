6e7736b92e13d11153f1d9c72b3f2ee20ef92781
iArctix <connortodd.ct@gmail.com>
2023-08-20 16:27:51 +0100

    Basic movement
    Basic Movement

---
+++ UnityProject/DaleTheDigger/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Check if the character is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        // Jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        float moveDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        // Flipping the sprite
+        if (moveDirection > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveDirection < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
