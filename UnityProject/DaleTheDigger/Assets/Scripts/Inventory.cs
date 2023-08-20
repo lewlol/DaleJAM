2a40627fe616d7154b9d505cc1d3f269372be9a9
iArctix <connortodd.ct@gmail.com>
2023-08-20 23:40:11 +0100

    Inventory stats set up
    sadasdsad

---
+++ UnityProject/DaleTheDigger/Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static int Rocks;
+    public static int Ores;
+    public static int Gemstones;
+
+    public static int Rockcoins;
+    public static int Orescoins;
+    public static int Gemstonecoins;
+    public static int Totalcoins;
+
+    private void Start()
+    {
+        Rocks = 0;
+        Ores = 0;
+        Gemstones = 0;
+        Rockcoins = 0;
+        Orescoins = 0;
+        Gemstonecoins = 0;
+    }
+
+    public static void newday()
+    {
+        Rocks = 0;
+        Ores = 0;
+        Gemstones = 0;
+        Rockcoins = 0;
+        Orescoins = 0;
+        Gemstonecoins = 0;
+    }
+}
