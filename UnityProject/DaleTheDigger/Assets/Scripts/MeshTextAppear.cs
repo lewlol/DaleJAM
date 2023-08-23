721bd87946033ad3e948bd48eb9646f740bfe577
lewlol <lewdevbusiness@gmail.com>
2023-08-23 18:00:37 +0100

    Highlight

--- UnityProject/DaleTheDigger/Assets/Scripts/MeshTextAppear.cs
+++ UnityProject/DaleTheDigger/Assets/Scripts/MeshTextAppear.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class MeshTextAppear : MonoBehaviour
 {
-    GameObject TextMesh;
+    public GameObject TextMesh;
     public void GenerateText(Vector2 position, float time, string text, float size)
     { 
         GameObject textMesh = Instantiate(TextMesh, position, Quaternion.identity);
         TextMeshProUGUI tmp = textMesh.GetComponent<TextMeshProUGUI>();
 
         tmp.text = text;
-        tmp.fontSize = size;
+        tmp.fontSize = size; //5 is normally a good size
         Destroy(textMesh, time);
     }
 }
