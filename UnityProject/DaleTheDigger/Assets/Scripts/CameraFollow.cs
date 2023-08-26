3b6cd3152a7ddde092dc3733783980a0d93d50e7
lewlol <lewdevbusiness@gmail.com>
2023-08-26 23:07:39 +0100

    swag

--- UnityProject/DaleTheDigger/Assets/Scripts/CameraFollow.cs
+++ UnityProject/DaleTheDigger/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,6 @@ public class CameraFollow : MonoBehaviour
     public Transform player;
     public void LateUpdate()
     {
-        transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(player.position.y, -296, 100), transform.position.z);
     }
 }
